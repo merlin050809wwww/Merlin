b3661d8fd78bc44e0b87c02098464155e7b8a58f
merlin050809 <merlin050809wwww@gmail.com>
2020-10-01 16:22:46 +0300

    000

--- FileStorage/Controllers/MoviesController.cs
+++ FileStorage/Controllers/MoviesController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FileStorage.Data;
 using FileStorage.Models;
@@ -14,9 +13,6 @@ using System.IO;
 using Microsoft.AspNetCore.Http;
 using System.IO.Compression;
 using Microsoft.AspNetCore.Hosting;
-using System.Runtime.CompilerServices;
-using System.Net.Mail;
-using System.Net;
 using Microsoft.Extensions.Logging;
 using FileStorage.Controllers;
 
